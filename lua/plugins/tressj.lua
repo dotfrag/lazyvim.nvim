0075729742407a531a8c2ad90cca61c7bb93b14a
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-05-10 21:35:48 +0300

    2023-05-10 21:35:48

---
+++ lua/plugins/tressj.lua
@@ -0,0 +1,17 @@
+return {
+  "Wansmer/treesj",
+  keys = {
+    { "<leader>m", "<cmd>TSJToggle<cr>", desc = "Node Toggle" },
+    { "<leader>gzs", "<cmd>TSJSplit<cr>", desc = "Node Split" },
+    { "<leader>gzj", "<cmd>TSJJoin<cr>", desc = "Node Join" },
+  },
+  cmd = {
+    "TSJToggle",
+    "TSJSplit",
+    "TSJJoin",
+  },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("treesj").setup({ use_default_keymaps = false })
+  end,
+}
