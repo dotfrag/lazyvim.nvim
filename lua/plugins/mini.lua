e8b9a8d51c4af02a1d89afe938306c2d101d1e00
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-05-15 11:14:43 +0300

    2023-05-15 11:14:43

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,12 @@
 return {
   {
     "echasnovski/mini.splitjoin",
+    event = "VeryLazy",
     config = true,
   },
   {
     "echasnovski/mini.align",
+    event = "VeryLazy",
     config = true,
   },
 }
