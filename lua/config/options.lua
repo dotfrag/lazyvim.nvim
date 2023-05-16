8ac3e17d0f0742cd3becf38068707c4dffc18816
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-05-16 17:09:01 +0300

    2023-05-16 17:09:01

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- Enable treesitter folding and disable folding at startup
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldenable = false
