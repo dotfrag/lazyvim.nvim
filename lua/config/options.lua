ac870d90937268e24a5df1121847b07fbdf2f9a6
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-05-19 16:54:01 +0300

    2023-05-19 16:54:01

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,12 @@
 -- Add any additional options here
 
 local opt = vim.opt
+local cmd = vim.cmd
 
 -- Enable treesitter folding and disable folding at startup
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldenable = false
+
+-- indent-blankline current context highlight
+cmd([[highlight IndentBlanklineContextStart guibg=#2d3f76]])
