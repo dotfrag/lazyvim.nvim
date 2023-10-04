99322170495dd3ce796bd055c4470dd20dea93e0
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-10-04 16:34:19 +0300

    2023-10-04 16:34:19

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,19 +1,5 @@
 return {
   {
-    "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "gsa",
-        delete = "gsd",
-        find = "gsf",
-        find_left = "gsF",
-        highlight = "gsh",
-        replace = "gsr",
-        update_n_lines = "gsn",
-      },
-    },
-  },
-  {
     "echasnovski/mini.splitjoin",
     event = "VeryLazy",
     config = true,
