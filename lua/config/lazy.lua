d74dfd8dd194249d15a12eede6fb905b7501c048
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-10-15 11:45:28 +0300

    2023-10-15 11:45:28

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,15 +11,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.mini-files" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.util.project" },
+    -- now using lazyvim.json instead
     -- import/override with your plugins
     { import = "plugins" },
   },
