4c0246bb3b35214346539fdae235e869e4ef7960
dotfrag <17456867+dotfrag@users.noreply.github.com>
2023-11-09 16:11:22 +0200

    2023-11-09 16:11:22

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,11 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  -- automatically check for plugin updates
+  checker = {
+    enabled = true,
+    notify = false,
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
