5e4a8c7c4ce165990cc38de5fed72acf679760f7
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-01-21 18:42:21 +0200

    2024-01-21 18:42:21

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -2,8 +2,8 @@ return {
   "rest-nvim/rest.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { "<leader>cr", "<Plug>RestNvim", desc = "Run request" },
-    { "<leader>cR", "<Plug>RestNvim", desc = "Run last request" },
-    { "<leader>cp", "<Plug>RestNvim", desc = "Preview request" },
+    { "<leader>cr", ft = "http", "<Plug>RestNvim", desc = "Run request" },
+    { "<leader>cR", ft = "http", "<Plug>RestNvim", desc = "Run last request" },
+    { "<leader>cp", ft = "http", "<Plug>RestNvim", desc = "Preview request" },
   },
 }
