098d1aa841e8257aacf996ddd93fbf2375584058
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-02-02 18:28:05 +0200

    2024-02-02 18:28:05

---
+++ lua/plugins/extras/ui/cmp-cmdline.lua
@@ -0,0 +1,14 @@
+return {
+  "hrsh7th/cmp-cmdline",
+  event = "VeryLazy",
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources(
+        { { name = "path" } },
+        { { name = "cmdline", option = { ignore_cmds = { "Man", "!" } } } }
+      ),
+    })
+  end,
+}
